f72b43539c2ae6fde20fd4fa4bf799a3a3fb1f77
yewin <yewin29081990@gmail.com>
2017-12-28 16:20:32 +0630

    travisCI test.

--- src/test/java/com/mep/MepApplicationTests.java
+++ src/test/java/com/mep/MepApplicationTests.java
@@ -8,14 +8,12 @@ import javax.sql.DataSource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.mep.util.TestAbstract;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class MepApplicationTests extends TestAbstract {
 
 	@Autowired
--- src/test/java/com/mep/util/TestAbstract.java
+++ src/test/java/com/mep/util/TestAbstract.java
@@ -3,11 +3,9 @@ package com.mep.util;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 //@Configuration
-@SpringBootTest
 //@PropertySource("classpath:application-test.properties")
 //@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
