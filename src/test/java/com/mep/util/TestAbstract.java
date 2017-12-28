f7ac27a4fdf844ede866fa66d8a5da57327bec93
yewin <yewin29081990@gmail.com>
2017-12-28 16:29:08 +0630

    travisCI test.

--- src/test/java/com/mep/util/TestAbstract.java
+++ src/test/java/com/mep/util/TestAbstract.java
@@ -1,6 +1,8 @@
 package com.mep.util;
 
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -10,6 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 //@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @AutoConfigureTestDatabase(replace=Replace.NONE)
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public abstract class TestAbstract {
 
 }
