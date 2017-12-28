ee19b891779ee8192e5604e36230cb98aa1de74d
yewin <yewin29081990@gmail.com>
2017-12-28 16:08:37 +0630

    travisCI db test updated.

--- src/test/java/com/mep/util/TestAbstract.java
+++ src/test/java/com/mep/util/TestAbstract.java
@@ -1,17 +1,17 @@
 package com.mep.util;
 
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@Configuration
+//@Configuration
 @SpringBootTest
-@PropertySource("classpath:application-test.properties")
-@ActiveProfiles("test")
+//@PropertySource("classpath:application-test.properties")
+//@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
+@AutoConfigureTestDatabase(replace=Replace.NONE)
 public abstract class TestAbstract {
 
 }
