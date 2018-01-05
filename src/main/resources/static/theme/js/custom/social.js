9485bdddec86cbab18d39a10c3415207c57bcfd6
yewin <yewin29081990@gmail.com>
2018-01-05 16:34:50 +0630

    Add face share dialog function with facebook javascript sdk is completed.

--- src/main/resources/static/theme/js/custom/social.js
+++ src/main/resources/static/theme/js/custom/social.js
@@ -1,7 +1,3 @@
-function fbShare(url) {
-	window.open("https://www.facebook.com/sharer/sharer.php?u=" + url, "Popup", "location,status,scrollbars,resizable,width=800, height=800");
-}
-
 function lkShare(url) {
 	window.open("http://www.linkedin.com/shareArticle?mini=true&url=" + url + "&hashtags=mep", "Popup", "location,status,scrollbars,resizable,width=800, height=800");
 }
