40cc2d3e64539f8cb6122adc90da977e400cafb6
yewin <yewin29081990@gmail.com>
2018-01-05 21:33:32 +0630

    Modify footer and linkedin share.

--- src/main/resources/static/theme/js/custom/social.js
+++ src/main/resources/static/theme/js/custom/social.js
@@ -1,7 +1,8 @@
-function lkShare(url) {
-	window.open("http://www.linkedin.com/shareArticle?mini=true&url=" + url + "&hashtags=mep", "Popup", "location,status,scrollbars,resizable,width=800, height=800");
+function lkShare(url, title) {
+	window.open("https://www.linkedin.com/shareArticle?mini=true&url=" + url + "&title=" + title +
+			"&summary=.&source=LinkedIn", "Popup", "location,status,scrollbars,resizable,width=800, height=800");
 }
 
-function ttShare(url, text) {
-	window.open("https://twitter.com/share?url=" + url + "&text=" + text + ":", "Popup", "location,status,scrollbars,resizable,width=800, height=800");
+function ttShare(url, title) {
+	window.open("https://twitter.com/share?url=" + url + "&text=" + title + ":", "Popup", "location,status,scrollbars,resizable,width=800, height=800");
 }
